c4dff51ab9ab93039bda186852f1723c8f3388ec
Михаил <espador@yandex.ru>
2018-05-14 13:43:21 +0300

    new

--- DrillDownPie/Controllers/HomeController.cs
+++ DrillDownPie/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DrillDownPie.Models;
+using Newtonsoft.Json;
 
 namespace DrillDownPie.Controllers
 {
@@ -17,26 +18,13 @@ namespace DrillDownPie.Controllers
             // передаем все объекты в динамическое свойство Books в ViewBag
             ViewBag.Books = books;
             // возвращаем представление
+           
+
             return View();
 
         }
-
-        [HttpGet]
-        public ActionResult Buy(int id)
-        {
-            ViewBag.BookId = id;
-            return View();
-        }
-        [HttpPost]
-        public string Buy(Purchase purchase)
-        {
-            purchase.Date = DateTime.Now;
-            // добавляем информацию о покупке в базу данных
-            db.Purchases.Add(purchase);
-            // сохраняем в бд все изменения
-            db.SaveChanges();
-            return "Спасибо," + purchase.Person + ", за покупку!";
-        }
+       
+      
 
     }
 }
--- DrillDownPie/Models/Book.cs
+++ DrillDownPie/Models/Book.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace DrillDownPie.Models
-{
+namespace DrillDownPie.Models { 
+[JsonObject]
         public class Book
         {
             // ID книги
+            [JsonProperty("id")]
             public int Id { get; set; }
             // название книги
             public string Name { get; set; }
